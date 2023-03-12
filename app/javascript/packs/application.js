82a2b48e9275df64ada38a6eb33b97a3365fb302
erikosampaio <erikoa.93@gmail.com>
2023-03-12 13:45:03 -0300

    Removo import de jquery com erro no carregamento do tooltip.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import $ from "jquery"
 import "bootstrap/dist/js/bootstrap.bundle"
 
 import "../src/application.css"
