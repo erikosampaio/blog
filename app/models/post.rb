415051e2c0d7d03c1318c39afee9b11b9ffa1fe5
erikosampaio <erikoa.93@gmail.com>
2023-03-12 13:43:05 -0300

    Adicionoo gem friendly_id para customização de url de acordo com nome do post + inicio confirguração de gem pg_search para pesquisa rápida postgresql.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,13 @@
 class Post < ApplicationRecord
+  include PgSearch
+  extend FriendlyId
   
   has_many :comments
   
   validates :title, :author, :body, presence: true
+
+  friendly_id :title, use: :slugged
+
+  # pg_search_scope :search,
+  #   against: %i[title author body]
 end
